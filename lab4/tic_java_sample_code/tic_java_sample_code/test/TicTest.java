28ff66e3612395ddb191cf42b83c4108cb808c0b
Markello <jleta084@uottawa.ca>
2023-06-28 20:59:19 -0400

    test1

---
+++ lab4/tic_java_sample_code/tic_java_sample_code/test/TicTest.java
@@ -0,0 +1,18 @@
+package tic;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TicTest {
+
+	@Test
+	void testEqual() {
+		Tic board = new Tic(3, 3);
+		board.move(0, 1);
+		Tic board2 = new Tic(3,3);
+		board2.move(1, 0);
+		assertEquals(board.equals(board2),  true);
+	}
+	
+}
